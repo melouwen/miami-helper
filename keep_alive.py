849838799fce8619970390687183f33e5fc36e29
melouwen <migalenkodenis@gmail.com>
2025-10-30 15:04:29 +0200

    1.7

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,14 @@
 from aiohttp import web
-import asyncio
+import threading
 
 async def handle(request):
     return web.Response(text="I'm alive")
 
-async def run():
+def run():
     app = web.Application()
-    app.router.add_get('/', handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, host="0.0.0.0", port=8080)
-    await site.start()
-    print("✅ Keep-alive server started on port 8080")
+    app.router.add_get("/", handle)
+    web.run_app(app, host="0.0.0.0", port=8080)
 
 def keep_alive():
-    loop = asyncio.get_event_loop()
-    loop.create_task(run())
+    t = threading.Thread(target=run)
+    t.start()
